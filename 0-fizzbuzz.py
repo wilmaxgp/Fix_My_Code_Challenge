a21e8e0da2160c66ce7f45f40e1d0989ae1b642e
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-03-25 07:59:18 +0100

    fixied All codes

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -34,6 +34,6 @@ if __name__ == '__main__':
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
+        
     number = int(sys.argv[1])
     fizzbuzz(number)
