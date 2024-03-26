4c6190d0430d5f02022ae9cb3f61812ed1b1c9e6
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-25 23:52:45 -0700

    Fixed deletion logic in delete_dnodeint_at_index

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,36 +11,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    dlistint_t *temp = NULL;
+    dlistint_t *saved_head;
+    dlistint_t *tmp;
     unsigned int p;
 
     if (*head == NULL)
-        return (-1);
-
-    if (index == 0)
     {
-        *head = current->next;
-        if (*head)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    for (p = 0; p < index && current != NULL; p++)
-    {
-        temp = current;
-        current = current->next;
-    }
-
-    if (current == NULL)
         return (-1);
-
-    temp->next = current->next;
-    if (current->next)
-        current->next->prev = temp;
-
-    free(current);
+    }
+    saved_head = *head;
+    p = 0;
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+    if (p != index)
+    {
+        *head = saved_head;
+        return (-1);
+    }
+    if (0 == index)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
+        {
+            tmp->prev = NULL;
+        }
+    }
+    else
+    {
+        (*head)->prev = (*head)->next;
+        free(*head);
+        if ((*head)->next)
+            (*head)->next->prev = (*head)->prev;
+        *head = saved_head;
+    }
     return (1);
 }
 
